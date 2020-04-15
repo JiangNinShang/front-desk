41cd1fbd3008b146ee728b505c4406da10851548
JiangNinShang <1753291617@qq.com>
2020-04-15 10:25:01 +0800

    一giao

--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-
+import axios from 'axios'
+import store from './vueX/user/store.js'
+import router from './router/index.js'
+import qs from 'qs'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App.vue'
+Vue.prototype.axios = axios
+Vue.use(ElementUI)
+Vue.prototype.$http = axios
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+Vue.prototype.$http.defaults.baseURL = 'http://localhost:8083/'
 Vue.config.productionTip = false
+Vue.prototype.$qs = qs
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  store,
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
