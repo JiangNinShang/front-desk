559dbcb3c14e1ad0e8579b46d93e64854f77c25d
JiangNinShang <1753291617@qq.com>
2020-05-20 09:26:11 +0800

    a

--- src/main.js
+++ src/main.js
@@ -1,21 +1,28 @@
 import Vue from 'vue'
-import axios from 'axios'
-import store from './vueX/user/store.js'
-import router from './router/index.js'
-import qs from 'qs'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import store from './vuex/trade and order/store.js'
+import router from './router/index.js'
+import ElementUI from 'element-ui'
+import axios from 'axios'
+import 'element-ui/lib/theme-chalk/index.css'
+import {
+	pcaa
+} from 'area-data-vue';
+import 'area-linkage-vue/dist/index.css';
+import AreaLinkageVue from 'area-linkage-vue';
 Vue.prototype.axios = axios
-Vue.use(ElementUI)
 Vue.prototype.$http = axios
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-Vue.prototype.$http.defaults.baseURL = 'http://localhost:8083/'
-Vue.config.productionTip = false
-Vue.prototype.$qs = qs
+Vue.prototype.$http.defaults.baseURL = 'http://localhost:8090/'
+Vue.prototype.$pcaa = pcaa;
 
+Vue.use(AreaLinkageVue)
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
 new Vue({
-  store,
-  router,
-  render: h => h(App)
+	store,
+	router,
+	render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,45 @@
+// 路由配置
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router)
+// 路由
+import VueRouter from 'vue-router' // 在Vue使用VueRouter
+import ShoppingT from '@/components/shopping trolley.vue'
+import order from '@/components/order.vue'
+import Head from '@/components/head.vue'
+import Footer from '@/components/footer.vue'
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+// 定义路由规则
+var routes = [{
+    name: 'home',
+    path: '/',
+    component: ShoppingT
+  },
+  {
+    name: 'ShoppingT',
+    path: '/ShoppingT',
+    component: ShoppingT
+  },
+  {
+    name: 'order',
+    path: '/order',
+    component: order
+  },
+  {
+    name: 'Head',
+    path: '/Head',
+    component: Head
+  },
+  {
+    name: 'Footer',
+    path: '/Footer',
+    component: Footer
+  }
+]
+// 创建router对象
+var router = new VueRouter({
+  routes
 })
+
+// 导出路由对象
+export default router
